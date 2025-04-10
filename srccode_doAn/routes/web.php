4a1fc2b0682a8b26b5c2a5dd31521dbca5d457f2
hophanminhdang <dangho8642@gmail.com>
2025-04-10 16:44:59 +0700

    a

---
+++ srccode_doAn/routes/web.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CrudUserController;
+use App\Http\Controllers\UserCrudController;
+use App\Http\Controllers\OrderController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+//Route::get('dashboard', [CrudUserController::class, 'dashboard']);
+
+//Route::get('login', [CrudUserController::class, 'login'])->name('login');
+//Route::post('login', [CrudUserController::class, 'authUser'])->name('user.authUser');
+
+//Route::get('create', [CrudUserController::class, 'createUser'])->name('user.createUser');
+//Route::post('create', [CrudUserController::class, 'postUser'])->name('user.postUser');
+
+//Route::get('read', [CrudUserController::class, 'readUser'])->name('user.readUser');
+
+//Route::get('delete', [CrudUserController::class, 'deleteUser'])->name('user.deleteUser');
+
+//Route::get('update', [CrudUserController::class, 'updateUser'])->name('user.updateUser');
+//Route::post('update', [CrudUserController::class, 'postUpdateUser'])->name('user.postUpdateUser');
+
+//Route::get('list', [CrudUserController::class, 'listUser'])->name('user.list');
+
+//Route::get('signout', [CrudUserController::class, 'signOut'])->name('signout');
+
+
+
+//route do an
+Route::get('/users', [UserCrudController::class, 'index'])->name('users.index');
+Route::get('UserCreate', [UserCrudController::class, 'create'])->name('users.UserCreate');
+Route::post('users', [UserCrudController::class, 'postUser'])->name('users.store');
+Route::get('/users/{id}/edit', [UserCrudController::class, 'editUser'])->name('users.editUser');
+Route::delete('/users/{id}/delete', [UserCrudController::class, 'deleteUser'])->name('users.deleteUser');
+Route::put('/users/{id}', [UserCrudController::class, 'updateUser'])->name('users.updateUser');
+
+
+Route::get('/orders', [OrderController::class, 'index'])->name('orders.index');
+Route::post('/orders', [OrderController::class, 'store'])->name('orders.store');
+Route::post('/orders/{id}', [OrderController::class, 'update'])->name('orders.update');
+Route::delete('/orders/{id}', [OrderController::class, 'destroy'])->name('orders.destroy');
+
+Route::get('/', function () {
+    return view('welcome');
+});
